c5d481df4230092ee82b1bb38040b7ebcd262c46
kintrovaviolka-dev <kintrovaviolka@gmail.com>
2026-07-22 01:17:01 +0200

    Add files via upload

--- urgentni-prijem/vite.config.ts
+++ urgentni-prijem/vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/urgentni-prijem/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
