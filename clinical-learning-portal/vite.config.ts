1ca691496e147c5695b1b2752e3b6edb6ee8e22e
kintrovaviolka-dev <kintrovaviolka@gmail.com>
2026-07-22 01:16:17 +0200

    Add files via upload

--- clinical-learning-portal/vite.config.ts
+++ clinical-learning-portal/vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/clinical-portal/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
