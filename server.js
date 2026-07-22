e94fe37fcb76b04093da7b1c22b4fae503862bc5
kintrovaviolka-dev <kintrovaviolka@gmail.com>
2026-07-22 15:49:31 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Serve built dist directories for Vite subapps
-app.use('/urgentni-prijem', express.static(path.join(__dirname, 'urgentni-prijem/dist')));
-app.use('/clinical-learning-portal', express.static(path.join(__dirname, 'clinical-learning-portal/dist')));
-app.use('/clinical-portal', express.static(path.join(__dirname, 'clinical-learning-portal/dist')));
+// Serve clinical-portal alias
+app.use('/clinical-portal', express.static(path.join(__dirname, 'clinical-learning-portal')));
 
 // Serve static files from root directory
 app.use(express.static(__dirname));
